0f7cecd7e182ec06dfbbf29d45747816fa8950d4
jcastagna <jeremie.castagna@gmail.com>
2011-07-07 15:48:00 -0700

    solved puzzle 12

--- 0011.c
+++ 0011.c
@@ -4,17 +4,16 @@
 #define MX 20
 #define MY 20
 
-void load_matrix(char *filename, int matrix[][MY]) {
-//  char *filename = "0011.txt";
-  FILE *file     = fopen(filename, "r");
+void load_matrix(char *filename, int matrix[MY][MX]) {
+  FILE *file = fopen(filename, "r");
 
   int cx = 0;
   int cy = 0;
 
   int c; while(c != EOF) {
     if( fscanf(file, "%2d", &(matrix[cy][cx])) > 0 ) {
-      printf("coord: x=%d", cx);
-      printf(" y=%d\n", cy);
+      //printf("coord: x=%d", cx);
+      //printf(" y=%d\n", cy);
       c = fgetc(file);
       switch(c) {
       case ' ': cx++; break;
@@ -28,9 +27,70 @@ void load_matrix(char *filename, int matrix[][MY]) {
   fclose(file);
 }
 
+
+// Calculate the product for a matrix in a given direction.
+
+int matrix_product(int x, int y, int len, int dir, int matrix[MY][MX]) {
+  int product = 1;
+  int i;
+
+  for(i = 0; i < len; i++) {
+
+    switch(dir) {
+    // vertical down
+    case 0:
+      if(y+len > MY) return 0;
+      product = product * matrix[y+i][x];
+      break;
+
+    // horizontal forward
+    case 1:
+      if(x+len > MX) return 0;
+      product = product * matrix[y][x+i];
+      break;
+
+    // diagonal up
+    case 2:
+      if(x < len-1 || y+len > MY) return 0;
+      product = product * matrix[y+i][x-i];
+      break;
+
+    // diagonal down
+    case 3:
+      if(x+len > MX || y+len > MY) return 0;
+      product = product * matrix[y+i][x+i];
+      break;
+    }
+  }
+
+  return product;
+}
+
+
+int highest_product(int len, int matrix[MY][MX]) {
+  int highest = 0;
+  int product;
+
+  int y; for(y = 0; y < MY; y++) {
+    int x; for(x = 0; x < MX; x++) {
+      int dir; for(dir = 0; dir < 4; dir++) {
+
+        product = matrix_product(x, y, len, dir, matrix);
+        if(product > highest) highest = product;
+      }
+    }
+  }
+
+  return highest;
+}
+
+
 int main(int argc, char *argv[]) {
   int matrix[MY][MX];
+  int prod_size = 4;
 
   load_matrix("0011.txt", matrix);
+  printf("%d\n", highest_product(prod_size, matrix));
+
   return 0;
 }
--- 0012.c
+++ 0012.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-int count_divs(long double number) {
+int count_divs(long number) {
   int divs = 0;
 
-  long double n; for(n = 1; n < number; n++){
+  long n; for(n = 1; n <= number; n++){
     if(fmod(number, n) == 0) {
-      divs++;
-      if(number / n < n) {
-        return divs * 2;
-      } else if(number / n == n) {
-        return divs * 2 - 1;
+      if((number / n) < n) {
+        return (divs * 2);
+      } else if((number / n) == n) {
+        return (divs * 2) - 1;
       }
+      divs++;
     }
   }
 
@@ -22,29 +22,17 @@ int count_divs(long double number) {
 int main(int argc, int *argv[]) {
   int min_div_count = 500;
   int div_count     = 0;
-  long double triangle      = 0;
-  long double min_number    = 1;
-
-  int n;
-
-  for(n = 1; n <= min_div_count; n++) {
-//printf("%Le\n", min_number);
-    min_number = min_number * n;
-  }
-
-
-  for(n = 1; n <= min_number; n++); triangle += n;
-
-  long double counter = min_number;
+  long triangle     = 0;
+  long min_number   = 1;
+  long counter      = 0;
 
   while(div_count <= min_div_count) {
     counter++;
     triangle  = triangle + counter;
     div_count = count_divs(triangle);
-    //printf("%d\n", div_count);
   }
 
-  printf("%Le\n", triangle);
+  printf("%ld\n", triangle);
 
   return 0;
 }
